068a6235645e37439f14cbcb7c4a100ce43ca3d7
Bartlomiej Obecny <bobecny@gmail.com>
2020-05-15 14:39:12 +0200

    chore: lint

--- scripts/check-npm.js
+++ scripts/check-npm.js
@@ -1,14 +1,16 @@
-const packageJson = require('package-json');
-const pJson = require('../package.json');
+const packageJson = require('package-json')
+const pJson = require('../package.json')
 
 packageJson(pJson.name.toLowerCase()).then((result) => {
   if (result.version === pJson.version) {
-    console.log('This version of package has been already released');
-    process.exit(1);
+    // eslint-disable-next-line no-console
+    console.log('This version of package has been already released')
+    process.exit(1)
   } else {
-    console.log(pJson.version);
-    process.exit(0);
+    // eslint-disable-next-line no-console
+    console.log(pJson.version)
+    process.exit(0)
   }
-}).catch((error) => {
-  process.exit(0);
-});
+}).catch(() => {
+  process.exit(0)
+})
--- scripts/check-version.js
+++ scripts/check-version.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
+const fs = require('fs')
+const os = require('os')
+const path = require('path')
 
-const appRoot = process.cwd();
-const packageJsonUrl = path.resolve(`${appRoot}/package.json`);
-const versionUrl = path.resolve(`${appRoot}/packages/dd-trace/lib/version.js`);
-const pJson = require(packageJsonUrl);
-const version = require(versionUrl);
+const appRoot = process.cwd()
+const packageJsonUrl = path.resolve(`${appRoot}/package.json`)
+const versionUrl = path.resolve(`${appRoot}/packages/dd-trace/lib/version.js`)
+const pJson = require(packageJsonUrl)
+const version = require(versionUrl)
 
 if (pJson.version !== version) {
-  const content = `module.exports = '${pJson.version}'\n`;
-  fs.writeFileSync(versionUrl, content.replace(/\n/g, os.EOL));
+  const content = `module.exports = '${pJson.version}'\n`
+  fs.writeFileSync(versionUrl, content.replace(/\n/g, os.EOL))
+  // eslint-disable-next-line no-console
   console.log(pJson.version)
-  process.exit(2);
+  process.exit(2)
 }
-process.exit(0);
+process.exit(0)
