e2f32d9f071299e4886e525f5ae012a9b416e064
Bartlomiej Obecny <bobecny@gmail.com>
2020-05-16 02:23:57 +0200

    chore: auto release

--- scripts/release-prepare.sh
+++ scripts/release-prepare.sh
@@ -20,6 +20,15 @@ if [ "$exitCode" = "2" ]; then
   echo "Updating \"version.js\" to version \"$version\""
   git commit "packages/dd-trace/lib/version.js" --amend --no-edit
 fi
+
+upstreamExists=`git remote -v | grep upstream_tmp_release | tail -1`
+if [ "$upstreamExists" != "" ]; then
+  git remote remove upstream_tmp_release
+fi
+git remote add upstream_tmp_release git@github.com:lightstep/ls-trace-js.git
+
 currentBranch=`git symbolic-ref --short -q HEAD`
-git push --set-upstream origin $currentBranch
-git push origin "v$version"
+git push --set-upstream upstream_tmp_release $currentBranch
+git push upstream_tmp_release "v$version"
+
+git remote remove upstream_tmp_release
